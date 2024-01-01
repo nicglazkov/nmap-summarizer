49dd3f34337b3421ecc3652753cf1e69c7785467
Nicholas Glazkov <nic@glazkov.com>
2023-12-31 16:50:39 -0800

    added NmapSummarizer element

---
+++ src/nmap-summarizer.js
@@ -0,0 +1,9 @@
+import { LitElement, html } from "lit";
+
+export class NmapSummarizer extends LitElement {
+  render() {
+    return html`summarizer goes here`;
+  }
+}
+
+window.customElements.define("nmap-summarizer", NmapSummarizer);
